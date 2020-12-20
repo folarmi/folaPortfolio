07cac66eaaaaf6b8cc13ac6ee378863551c4360e
folarmi <fola4ril@gmail.com>
2020-12-20 22:36:11 +0000

    added contact button

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
 import moviedb from "./../images/moviedb.png";
@@ -12,7 +13,7 @@ const Projects = () => {
     <div>
       <Header />
       <section className="text-gray-700 body-font">
-        <div className="container px-5 py-24 mx-auto">
+        <div className="container px-5 py-24 mx-auto pb-4">
           <div className="flex flex-wrap -m-4 text-center">
             <div className="p-4 md:w-1/4 sm:w-1/2 w-4/5 ml-8">
               <a
@@ -26,7 +27,7 @@ const Projects = () => {
                   className="project-img rounded-lg transform transition duration-500 hover:scale-110"
                 />
               </a>
-              <p className="text-center uppercase mt-2 weight-500 text-3xl">
+              <p className="text-center uppercase mt-4 font-medium text-base">
                 Movie db
               </p>
             </div>
@@ -42,7 +43,7 @@ const Projects = () => {
                   className="project-img rounded-lg transform transition duration-500 hover:scale-110"
                 />
               </a>
-              <p className="text-center uppercase mt-2 weight-500 text-3xl">
+              <p className="text-center uppercase mt-4 font-medium text-base">
                 country search
               </p>
             </div>
@@ -58,7 +59,7 @@ const Projects = () => {
                   className="project-img rounded-lg transform transition duration-500 hover:scale-110"
                 />
               </a>
-              <p className="text-center uppercase mt-2 weight-500 text-3xl">
+              <p className="text-center uppercase mt-4 font-medium text-base">
                 menus
               </p>
             </div>
@@ -74,7 +75,7 @@ const Projects = () => {
                   className="project-img rounded-lg transform transition duration-500 hover:scale-110"
                 />
               </a>
-              <p className="text-center uppercase mt-2 weight-500 text-3xl">
+              <p className="text-center uppercase mt-4 font-medium text-base">
                 tours
               </p>
             </div>
@@ -90,7 +91,7 @@ const Projects = () => {
                   className="project-img rounded-lg transform transition duration-500 hover:scale-110"
                 />
               </a>
-              <p className="text-center uppercase mt-2 weight-500 text-3xl">
+              <p className="text-center uppercase mt-4 font-medium text-base">
                 tabs
               </p>
             </div>
@@ -106,13 +107,16 @@ const Projects = () => {
                   className="project-img rounded-lg transform transition duration-500 hover:scale-110"
                 />
               </a>
-              <p className="text-center uppercase mt-2 weight-500 text-3xl">
+              <p className="text-center uppercase mt-4 font-medium text-base">
                 shortly
               </p>
             </div>
           </div>
         </div>
       </section>
+      <button className="h-12 px-6 text-sm  text-indigo-100 ml-32 mb-8 transition-colors duration-150 bg-black rounded-lg focus:shadow-outline hover:bg-indigo-800">
+        <Link to="/contact">Contact Me</Link>
+      </button>
       <Footer />
     </div>
   );
